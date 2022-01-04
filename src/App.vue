41c59933a8e13723f1d618cbf45cf1ab5da7f981
Geonsang Yoo <geonsangyoo@gmail.com>
2022-01-05 05:11:03 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="main">
+    <v-container class="mt-2">
+      <v-row justify="center">
+        <v-col cols="12" style="z-index: 5" class="pb-0" align="center">
+          <v-img
+            lazy-src="./assets/dailybear_text.png"
+            max-width="400"
+            max-height="70"
+            src="./assets/dailybear_text.png"
+          ></v-img>
+        </v-col>
+        <v-col
+          style="width: 500px; position: absolute"
+          class="pt-2"
+          align="left"
+        >
+          <v-img
+            lazy-src="./assets/dailybear_left_img.png"
+            max-width="116"
+            max-height="80"
+            src="./assets/dailybear_left_img.png"
+          ></v-img>
+        </v-col>
+        <v-col
+          style="width: 500px; position: absolute"
+          class="mt-2"
+          align="right"
+        >
+          <v-img
+            lazy-src="./assets/dailybear_right_img.png"
+            max-width="50"
+            max-height="57"
+            src="./assets/dailybear_right_img.png"
+          ></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-main>
+      <Intro />
+      <div style="height: 100px" />
+    </v-main>
+    <v-footer fixed padless>
+      <v-card flat tile width="100%" class="yellow lighten-5 text-center">
+        <v-card-text class="grey--text text-overline">
+          {{ new Date().getFullYear() }} -
+          <strong>Daily Bear</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Intro from "./components/Intro";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    Intro,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+#main {
+  background-image: url("./assets/background.png");
+  background-size: cover;
 }
 </style>
---
+++ src/components/Intro.vue
@@ -0,0 +1,137 @@
+<template>
+  <v-container>
+    <div style="height: 20px"></div>
+    <v-row justify="center">
+      <v-col cols="12" align="center">
+        <v-btn
+          class="mx-2"
+          fab
+          small
+          color="brown darken-2"
+          href="mailto:dailybear.qna@google.com"
+          target="_blank"
+        >
+          <v-icon color="white"> mdi-email </v-icon>
+        </v-btn>
+        <v-btn
+          class="mx-2"
+          fab
+          small
+          color="brown darken-2"
+          href="https://github.com/geonsangyoo/dailybear"
+          target="_blank"
+        >
+          <v-icon color="white"> mdi-github </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12">
+        <p class="my-2 text-h4 font-weight-thin text-center">
+          <span>Pick your bear every day!</span>
+        </p>
+        <div style="height: 10px"></div>
+        <v-img
+          :src="require('../assets/screenshot65_1.png')"
+          class="my-3"
+          contain
+          height="800"
+        />
+      </v-col>
+      <div style="height: 30px"></div>
+      <v-col cols="12">
+        <p class="my-2 text-h4 font-weight-thin text-center">
+          <span>Write your own story every day!</span>
+        </p>
+        <div style="height: 10px"></div>
+        <v-img
+          :src="require('../assets/screenshot65_2.png')"
+          class="my-3"
+          contain
+          height="800"
+        />
+      </v-col>
+      <div style="height: 30px"></div>
+      <v-col cols="12">
+        <p class="my-2 text-h4 font-weight-thin text-center">
+          <span>Check out your statistic view of bear every month!</span>
+        </p>
+        <div style="height: 10px"></div>
+        <v-img
+          :src="require('../assets/screenshot65_3.png')"
+          class="my-3"
+          contain
+          height="800"
+        />
+      </v-col>
+      <div style="height: 30px"></div>
+      <v-col cols="12">
+        <p class="my-2 text-h4 font-weight-thin text-center">
+          <span>The List view with your story & bear get ready!</span>
+        </p>
+        <div style="height: 10px"></div>
+        <v-img
+          :src="require('../assets/screenshot65_4.png')"
+          class="my-3"
+          contain
+          height="800"
+        />
+      </v-col>
+      <div style="height: 30px"></div>
+      <v-col cols="12">
+        <p class="my-2 text-h4 font-weight-thin text-center">
+          <span>The List view with your story & bear get ready!</span>
+        </p>
+        <div style="height: 10px"></div>
+        <v-img
+          :src="require('../assets/screenshot65_4.png')"
+          class="my-3"
+          contain
+          height="800"
+        />
+      </v-col>
+      <div style="height: 30px"></div>
+      <v-col cols="12">
+        <p class="my-2 text-h4 font-weight-thin text-center">
+          <span>Change your font!</span>
+        </p>
+        <div style="height: 10px"></div>
+        <v-img
+          :src="require('../assets/screenshot65_5.png')"
+          class="my-3"
+          contain
+          height="800"
+        />
+      </v-col>
+      <div style="height: 30px"></div>
+      <v-col cols="12">
+        <p class="my-2 text-h4 font-weight-thin text-center">
+          <span>Share your story!</span>
+        </p>
+        <div style="height: 10px"></div>
+        <v-img
+          :src="require('../assets/screenshot65_6.png')"
+          class="my-3"
+          contain
+          height="800"
+        />
+      </v-col>
+      <div style="height: 30px"></div>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "intro",
+
+  data: () => ({}),
+};
+</script>
+
+<style scoped>
+span {
+  border-bottom: 3px solid #ffff58;
+  box-shadow: inset 0 -18px 0 #ffff58;
+}
+</style>
